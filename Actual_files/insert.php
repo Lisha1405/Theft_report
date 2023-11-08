e2ca691bdcc1112ba20101673ff7400bdfceadbe
samyuktha2005 <samyukthajaggaiahgari22@gmail.com>
2023-11-08 09:54:37 +0530

    update insert.php

--- Actual_files/insert.php
+++ Actual_files/insert.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
 <html>
-    
-<?php include 'navbar.html'; ?>
 
 <head>
     <style>
-           
-
         body {
             display: grid;
             justify-content: center;
-            align-items: center;  
+            align-items: center;
             background-image: url("https://i.pinimg.com/564x/e2/bd/9d/e2bd9d02fe736ac633db47c7b57ced35.jpg");
-        
+            background-repeat: no-repeat;
+            background-size: cover;
             height: 100vh;
             margin: 0;
             box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
-            
         }
 
         table {
-            background-color:lightblue;
             width: 1020px;
-            /* Set the width of the template */
+            background-color: rgba(240, 224, 190, 0.7);
         }
 
         td,
@@ -30,22 +25,11 @@
             color: rgb(0, 0, 0);
             font-size: 20px;
             padding: 10px;
-
         }
 
         .row {
-            background-color:lightblue;
+            background-color: rgba(240, 224, 190, 0.7);
             text-align: left;
-
-        }
-
-        .al {
-            position: relative;
-            text-align: right;
-        }
-
-        .td1 {
-            text-align: right;
         }
 
         .td2 {
@@ -53,17 +37,13 @@
             width: 25px;
         }
 
-        .td3 {
-            padding: 20px;
-        }
-
         sub {
-            background-color: rgb(134, 209, 134);
+            background-color: rgba(255, 230, 170, 0.7);
         }
 
         a:link,
         a:visited {
-            background-color:light blue;
+            background-color: orange;
             color: white;
             padding: 14px 25px;
             text-align: center;
@@ -89,20 +69,15 @@
 
         .a1:hover,
         .a1:active {
-            background-color:white;
+            background-color: white;
             float: right;
         }
     </style>
 </head>
 
 <body>
-    <form action="conect_f.php" method="POST">
-        <div class="de">
-
-    </div>
-    <div>
+    <form action="conect_f.php" method="POST" onsubmit="return validateForm()">
         <table>
-
             <tr class="row">
                 <td colspan="30">
                     FILE FIR
@@ -110,37 +85,27 @@
             </tr>
             <tr>
                 <td>
-                    <span style="font-size: 20px; text-align: center;font-family:Verdana, Geneva, Tahoma, sans-serif;font-weight: 200;">DETAILS OF COMPLAINT/INFORMATION TO POLICE:</span>
+                    <span style="font-size: 20px; text-align: center;font-family: Verdana, Geneva, Tahoma, sans-serif;font-weight: 200;">DETAILS OF COMPLAINT/INFORMATION TO POLICE:</span>
                 </td>
             </tr>
             <tr>
                 <td class="td2">
                     First name
                 </td>
-
-                
-            </tr>
-            <tr>
-
-                
-                </td>
                 <td class="td2">
-                    <label>
-                        <input type="text" name="firstname" id="firstname" placeholder="First Name" required>
-                    </label>
+                    Age
                 </td>
-
-            </tr>
-
-            <tr>
-                <td class="td2"> Age</td>
                 <td class="td2">
                     Gender
                 </td>
             </tr>
             <tr>
                 <td class="td2">
-
+                    <label>
+                        <input type="text" name="firstname" id="firstname" placeholder="First Name" required>
+                    </label>
+                </td>
+                <td class="td2">
                     <label>
                         <input type="number" name="a" id="a" placeholder="Age" required>
                     </label>
@@ -151,7 +116,6 @@
                     </label>
                 </td>
             </tr>
-
             <tr>
                 <td class="td2">
                     State:
@@ -159,21 +123,6 @@
                 <td class="td2">
                     Phone no:
                 </td>
-            </tr>
-            <tr>
-                <td class="td2">
-                    <label>
-                        <input type="text" name="s" id="s" placeholder="State" required>
-                    </label>
-                </td>
-                <td class="td2">
-                    <label>
-
-                        <input type="text" name="ph" id="ph" maxlength="10" placeholder="Enter phone number" required>
-                    </label>
-                </td>
-            </tr>
-            <tr>
                 <td class="td2">
                     Occupation:
                 </td>
@@ -184,85 +133,86 @@
             <tr>
                 <td class="td2">
                     <label>
+                        <input type="text" name="s" id="s" placeholder="State" required>
+                    </label>
+                </td>
+                <td class="td2">
+                    <label>
+                        <input type="text" name="ph" id="ph" maxlength="10" placeholder="Enter phone number" required>
+                    </label>
+                </td>
+                <td class="td2">
+                    <label>
                         <input type="text" name="oc" id="oc" placeholder="Occupation" required>
                     </label>
                 </td>
-                <td>
+                <td class="td2">
                     <label>
                         <input type="text" name="na" id="na" placeholder="Nationality" required>
                     </label>
                 </td>
             </tr>
-
             <tr>
-                <td>
+                <td class="td2">
                     DOF:
                 </td>
-                <td>
+                <td class="td2">
                     Stolen Date:
                 </td>
-            </tr>
-            <tr>
-                <td>
-                    <label>
-                        <input type="date" name="DOF" id="DOF" placeholder="DOF" required>
-                    </label>
-                </td>
-                <td>
-                    <label>
-                        <input type="date" name="Stolen_date" id="Stolen date" placeholder="Stolen Date" required>
-                    </label>
-                </td>
-                
-                <!-- <td>
-                <label>
-                        <input type="text" name="Stolen_time" id="Stolen time" placeholder="Stolen time">
-                    </label>
-                </td> -->
-
-            </tr>
-            <tr>
-                <td class="align">
+                <td class="td2">
                     Type:
                 </td>
-                <td class="align">
+                <td class="td2">
                     Color:
                 </td>
             </tr>
             <tr>
-                <td>
-                    <input type="text" name="type" id="type" placeholder="Type" required>
+                <td class="td2">
+                    <label>
+                        <input type="date" name="DOF" id="DOF" placeholder="DOF" required>
+                    </label>
                 </td>
-                <td>
-                    <input type="text" name="color" id="color" placeholder="Color" required>
+                <td class="td2">
+                    <label>
+                        <input type="date" name="Stolen_date" id="Stolen_date" placeholder="Stolen Date" required>
+                    </label>
+                </td>
+                <td class="td2">
+                    <label>
+                        <input type="text" name="type" id="type" placeholder="Type" required>
+                    </label>
+                </td>
+                <td class="td2">
+                    <label>
+                        <input type="text" name="color" id="color" placeholder="Color" required>
+                    </label>
                 </td>
             </tr>
             <tr>
-                <td>
+                <td class="td2">
                     Number:
                 </td>
-                <td>
+                <td class="td2">
                     Value:
                 </td>
             </tr>
             <tr>
-                <td>
-                    <input type="number" name="number" id="number" placeholder="Number" required>
+                <td class="td2">
+                    <label>
+                        <input type="number" name="number" id="number" placeholder="Number" required>
+                    </label>
                 </td>
-                <td>
-                    <input type="number" name="value" id="value" placeholder="Value" required>
+                <td class="td2">
+                    <label>
+                        <input type="number" name="value" id="value" placeholder="Value" required>
+                    </label>
                 </td>
             </tr>
             <tr>
                 <td class="al td2">
-
-                    <input type="checkbox" id="showDetails" name="checkbox" required>Having a suspect
-
-
-
+                    <input type="checkbox" id="showDetails" name="checkbox"> Having a suspect
                 </td>
                 <td>
-
                 </td>
             </tr>
             <tr>
@@ -290,31 +240,115 @@
                         <br>
                         <label for="occ">Occupation:</label>
                         <br>
-                        <input type="text" name="S_Occuaption" id="S_Occuaption" placeholder="Occuaption" required>
+                        <input type="text" name="S_Occuaption" id="S_Occuaption" placeholder="Occupation" required>
                         <br>
                         <label for="nat">Nationality:</label>
                         <br>
                         <input type="text" name="S_Nationality" id="S_Nationality" placeholder="Nationality" required>
-
                     </div>
-                    
-
                 </td>
             </tr>
             <tr class="row">
                 <td colspan="30">
-                    <input type="reset" value="reset" name = "reset" required>
-                    <input type="submit" value="submit" name = "submit" required>
-
+                    <input type="reset" value="Reset" name="reset" required>
+                    <input type="submit" value="Submit" name="submit" required>
                 </td>
             </tr>
-        
         </table>
-    </div>
     </form>
     <script>
         const showDetailsCheckbox = document.getElementById("showDetails");
         const detailsSection = document.getElementById("detailsSection");
+
+        function validateForm() {
+            var firstname = document.getElementById("firstname").value;
+            var a = document.getElementById("a").value;
+            var g = document.getElementById("g").value;
+            var s = document.getElementById("s").value;
+            var ph = document.getElementById("ph").value;
+            var oc = document.getElementById("oc").value;
+            var na = document.getElementById("na").value;
+            var DOF = document.getElementById("DOF").value;
+            var Stolen_date = document.getElementById("Stolen_date").value;
+            var type = document.getElementById("type").value;
+            var color = document.getElementById("color").value;
+            var number = document.getElementById("number").value;
+            var value = document.getElementById("value").value;
+
+            // Check if all required fields are filled out
+            if (
+                firstname == "" ||
+                a == "" ||
+                g == "" ||
+                s == "" ||
+                ph == "" ||
+                oc == "" ||
+                na == "" ||
+                DOF == "" ||
+                Stolen_date == "" ||
+                type == "" ||
+                color == "" ||
+                number == "" ||
+                value == ""
+            ) {
+                alert("All fields must be filled out");
+                return false;
+            }
+
+            // Check if age is less than 100
+            if (parseInt(a) >= 100) {
+                alert("Age must be less than 100");
+                return false;
+            }
+
+            // Check if color is a valid color name (you can customize this regex)
+            var colorRegex = /^[a-zA-Z]+$/;
+            if (!color.match(colorRegex)) {
+                alert("Color must be a valid color name");
+                return false;
+            }
+
+            // Check if gender is either male, female, or other
+            if (
+                !(g.toLowerCase() === "male" ||
+                g.toLowerCase() === "female" ||
+                g.toLowerCase() === "other")
+            ) {
+                alert("Gender must be either Male, Female, or Other");
+                return false;
+            }
+
+            // Check if the checkbox is selected
+            if (showDetailsCheckbox.checked) {
+                // If checkbox is selected, validate suspect details
+                var S_name = document.getElementById("S_name").value;
+                var S_phone = document.getElementById("S_phone").value;
+                var S_age = document.getElementById("S_age").value;
+                var S_Gender = document.getElementById("S_Gender").value;
+                var S_State = document.getElementById("S_State").value;
+                var S_Occuaption = document.getElementById("S_Occuaption").value;
+                var S_Nationality = document.getElementById("S_Nationality").value;
+
+                // Check if suspect details are filled out
+                if (
+                    S_name == "" ||
+                    S_phone == "" ||
+                    S_age == "" ||
+                    S_Gender == "" ||
+                    S_State == "" ||
+                    S_Occuaption == "" ||
+                    S_Nationality == ""
+                ) {
+                    alert("If having a suspect, all suspect details must be filled out");
+                    return false;
+                }
+            }
+            if (parseInt(S_age) >= 100) {
+                alert("Age must be less than 100");
+                return false;
+            }
+            return true;
+        }
 
         showDetailsCheckbox.addEventListener("click", function () {
             if (showDetailsCheckbox.checked) {
